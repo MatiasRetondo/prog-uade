0442376b8851c76a5b8a3414dbed127ebc90fe3d
Matias <matiasezeretondo@gmail.com>
2022-09-23 17:12:12 -0300

    Fix and comment

---
+++ TP0/tp0_E1.py
@@ -0,0 +1,156 @@
+from importlib.machinery import SourceFileLoader
+
+def calculoBase(matriz):
+    #10% del sueldo base si ausentismos es igual a 0
+    #5% del sueldo base por cada hijo
+    arraySueldo = []
+    for i in range(len(matriz)):
+        sueldo = matriz[i][1]
+        print("sueldo", sueldo)
+        if matriz[i][2] >= 15:
+            sueldo += (sueldo * 0.12)
+        ausentismo = matriz[i][3]
+        hijos = matriz[i][4]
+        if ausentismo == 0:
+            sueldo += (sueldo * 0.1)
+        sueldo += (hijos * (sueldo * 0.05))
+        arraySueldo.append(sueldo)
+        matriz[i][1] = sueldo
+        print("la base imponible de:" + str(matriz[i][0]) + " es de: " + str(arraySueldo[i]))
+    return arraySueldo
+
+def ingresa_valida(min, max, msg):
+    data = int(input(msg))
+    while data < min or data > max:
+        data = int(input(msg))
+    return data
+
+def cargaUser(matriz):
+    condicion = True
+    values = []
+    while condicion == True:
+        print("Ingrtese numero de legajo")
+        legajo = int(input())
+        if legajo == 99999:
+            condicion = False
+        else:
+            while legajo < 10000 or legajo > 25000 or legajo in values:
+                print("El legajo debe estar entre 10000 y 25000")
+                print("Ingrese el codigo del legajo")
+                legajo = int(input())
+            values.append(legajo)
+            print("Ingrese el sueldo base")
+            sueldo = int(input())
+            while sueldo < 0:
+                print("El sueldo debe ser mayor a 0")
+                print("Ingrese el sueldo base")
+                sueldo = int(input())
+            values.append(sueldo)
+            print("Ingrese la antiguedad")
+            antiguedad = int(input())
+            while antiguedad < 0:
+                print("La antiguedad debe ser mayor a 0")
+                print("Ingrese la antiguedad")
+                antiguedad = int(input())
+            values.append(antiguedad)
+            print("Ingrese los dias de ausentismo")
+            ausentismo = int(input())
+            while ausentismo < 0:
+                print("La ausentismo debe ser mayor o igual a 0")
+                print("Ingrese los dias de ausentimo")
+                ausentismo = int(input())
+            values.append(ausentismo)
+            print("Ingrese la cantidad de hijos")
+            hijos = int(input())
+            values.append(hijos)
+            for i in range(len(values)):
+                matriz.append(values[i])
+
+            
+def muestraMatriz(matriz):
+    for f in range(len(matriz)):
+        for c in range(len(matriz[f])):
+            print(matriz[f][c], end=" ")
+        print()
+
+def muestraMatrizParalela(matriz, arraySueldo):
+    for f in range(len(matriz)):
+        print("Legajo: " + str(matriz[f][0]) + " Sueldo: " + str(matriz[f][1]) + " Base Imponible: " + str(arraySueldo[f]))
+
+
+def calculaPromedioArray(array):
+    promedio = 0
+    for i in range(len(array)):
+        promedio += array[i]
+    promedio = promedio / len(array)
+    return promedio
+
+def calculoMedicina(matriz, arraySueldo):
+    medicina = []
+    valor = 0
+    for f in range(len(matriz)):
+        valor = arraySueldo[f] * 0.07
+        medicina.append(valor)
+        print("el total a pagar de medicina de Legajo: " + str(matriz[f][0]) + " es de: " + str(valor))
+        if matriz[f][2] < 15:
+            matriz[f][1] = matriz[f][1] - valor
+    print("El total a pagar de medicina es de: " + str(sum(medicina)))
+    print("El promedio a pagar de medicina es de: " + str(calculaPromedioArray(medicina)))
+
+    
+def orderAndSlice(matriz):
+    arraySueldo = []
+    arrayAntiguedad = []
+    for i in range(len(matriz)):
+        arraySueldo.append(matriz[i][1])
+        arrayAntiguedad.append(matriz[i][2])
+        print("sueldo:", arrayAntiguedad , "antiguedad:", arraySueldo)
+    for i in range(len(arraySueldo)):
+        for j in range(len(arraySueldo)):
+            if arrayAntiguedad[i] < arrayAntiguedad[j]:
+                aux = arrayAntiguedad[i]
+                arrayAntiguedad[i] = arrayAntiguedad[j]
+                arrayAntiguedad[j] = aux
+                aux2 = arraySueldo[i]
+                arraySueldo[i] = arraySueldo[j]
+                arraySueldo[j] = aux2
+    if (len(arraySueldo) % 2) == 0:
+        mitad = int(len(arraySueldo) / 2)
+        print("array", arraySueldo)
+        print("array", arrayAntiguedad)
+        print("Los sueldos con mayor antiguedad son ", arraySueldo[:mitad])
+        print("Los sueldos con menor antiguedad son ", arraySueldo[mitad:])
+        
+                
+
+def __main__():  # definimos la funcion main
+
+    matriz = []
+    arraySueldo = []
+    cargaUser(matriz)
+    print("---------------------")
+    #Punto 2 - Calculo de sueldo base
+    arraySueldo = calculoBase(matriz)
+    print("---------------------")
+    print(matriz)
+
+    muestraMatrizParalela(matriz, arraySueldo)
+    print("---------------------")
+
+    #Puntos 3 y 4 - Calculo medicina prepaga y promedio de gasto medicina
+    calculoMedicina(matriz, arraySueldo)
+    print("---------------------")
+
+    #Punto 5 - Ordenar y mostrar los sueldos de los empleados con mayor y menor antiguedad
+    orderAndSlice(matriz)
+    
+
+
+#Esta mal la ejecucion del create, deberia haber sido un un for para las columnas con i en range(5), eso hubiera cargado bien la matriz
+#El resto esta bien, menos el orderAndSlice que no esta ordenando bien, bah creo que no esta bien porque la matriz esta mal cargada
+#tecnicamente no es una matriz, es una lista de listas, pero no es una matriz
+
+
+
+if __name__ == "__main__": 
+    __main__()
---
+++ TP0/tp0_E2.py
@@ -0,0 +1,104 @@
+
+def validaAlumnos(matriz, alumnos, curso, comision):
+    if matriz[curso][comision] + alumnos <= 30:
+        return True
+    else:
+        return False
+
+
+def cargaComisiones(matriz, cursos):
+    alumnos = 0
+    while alumnos != -1:
+        print("Ingrese la cantidad de alumnos")
+        alumnos = int(input())
+        if alumnos != -1:
+            curso = ingresa_valida(10000, 99999, "Ingrese el codigo del curso")
+            posCurso = cursos.index(curso)
+            comision = ingresa_valida(1, 15, "Ingrese la comision")
+            if validaAlumnos(matriz, alumnos, posCurso, comision):
+                matriz[posCurso][comision-1] += alumnos
+            else:
+                print("No hay cupo en la comision", comision, "del curso", curso)
+            
+
+
+def ingresa_valida(min, max, msg):
+    data = int(input(msg))
+    while data < min or data > max:
+        data = int(input(msg))
+    return data
+
+
+def cargaCodigo(cursos):
+    for i in range(10):
+        print("Ingrese el codigo del curso", i+1)
+        cursos[i] = int(input())
+        while cursos[i] < 10000 or cursos[i] > 99999:
+            print("El codigo debe ser de 5 digitos")
+            print("Ingrese el codigo del curso", i+1)
+            cursos[i] = int(input())
+
+
+def muestraMatriz(matriz):
+    for f in range(len(matriz)):
+        for c in range(len(matriz[f])):
+            print(matriz[f][c], end=" ")
+        print()
+
+
+#
+def cursosMasCargados(matriz, cursos):
+    valor = 0
+    valores= []
+    pos = []
+    for f in range(len(matriz)):
+        valor = max(matriz[f])
+        if valor > 0:
+            valores.append(valor)
+            pos.append(matriz[f].index(valor))
+            print("El curso", cursos[f], "tiene la comision", matriz[f].index(valor) + 1, "con", valor, "alumnos")
+    print("El curso con mas alumnos es el", cursos[valores.index(max(valores))], "con", max(valores), "alumnos en la comision", pos[valores.index(max(valores))]) 
+    print("")
+
+
+def cursosPocosAlumnos(matriz, cursos): 
+    for f in range(len(matriz)):
+        pos = []
+        for c in range(len(matriz[f])):
+            if matriz[f][c] <= 10:
+                pos.append(c)
+        print("El curso", cursos[f], "tiene las comisiones", pos, "con menos de 10 alumnos")
+    print("")
+    
+        
+            
+    
+
+def __main__():  # definimos la funcion main
+    cursos = [0] * 10
+    matriz = []
+    filas = 10
+    columnas = 15
+
+    for f in range(filas):
+        matriz.append([0]*columnas)
+
+    #carga de cursos
+    cargaCodigo(cursos)
+    cargaComisiones(matriz, cursos)
+
+    #muestra de los cursos sin alumnos (se puede hacer de mas formas pero serian muchos logs)
+    muestraMatriz(matriz)
+    print("-----------------------------")
+
+    #muestra las comisiones con mas alumnos de cada curso
+    cursosMasCargados(matriz, cursos)
+    print("-----------------------------")
+    #muestra las comisiones con menos de 10 alumnos de cada curso
+    cursosPocosAlumnos(matriz, cursos)
+
+
+
+
+if __name__ == "__main__":  # al hacer esto, le decimos que se ejecute automaticamente la funcion main al principio de ejecucion
+    __main__()
