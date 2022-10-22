59cc52cf392d6465a89b40ae5fe7020e8844768c
MatiasRetondo <matiasezeretondo@gmail.com>
2022-10-21 21:59:40 -0300

    archivo de telefono no esta terminado

---
+++ archivos/ejercicioGuiaT/index.py
@@ -0,0 +1,88 @@
+path = "archivos\\ejercicioGuiaT\\listado.txt"
+import random
+import string
+
+def get_random_string(length):
+    letters = string.ascii_lowercase #genera un caracter random de la a a la z
+    result_str = ''.join(random.choice(letters) for i in range(length)) #genera un string de longitud length
+    return result_str
+
+# Escribir un programa para gestionar un listado telefónico con los nombres y los teléfonos de los clientes de una empresa.
+
+# El programa debe incorporar funciones crear el archivo con el listado si no existe, para consultar el teléfono de un cliente,
+# añadir el teléfono de un nuevo cliente y eliminar el teléfono de un cliente.
+# El listado debe estar guardado en el archivo de texto listado.txt donde el nombre del cliente y su teléfono deben aparecer separados por comas
+# y cada cliente en una línea distinta. Utilizando excepciones, controlar que el usuario introduzca los datos correctos.
+
+def crearArchivo():
+    try:
+        arch = open(path, "wt")
+        arch.close()
+    except FileExistsError:
+        print("El archivo ya existe")
+    except:
+        print("Error desconocido")
+
+def consultarTelefono():
+    try:
+        arch = open(path, "rt")
+        nombre = input("Ingrese el nombre del cliente: ")
+        for line in arch.readlines():
+            if line.split(",")[0] == nombre:
+                print("El telefono del cliente es: " + line.split(",")[1])
+        arch.close()
+    except FileNotFoundError:
+        print("El archivo no existe")
+    except:
+        print("Error desconocido")
+
+def copiarArchivo():
+    try:
+        arch = open(path, "rt")
+        archTemp = open("archivos\\ejercicioGuiaT\\listadoTemp.txt", "wt")
+        for line in arch.readlines():
+            archTemp.write(line)
+        arch.close()
+        archTemp.close()
+    except FileNotFoundError:
+        print("El archivo no existe")
+    except:
+        print("Error desconocido")
+
+def agregarTelefono():
+    try:
+        arch = open(path, "wt") #esto borraria el archivo y lo reescribiria con el nuevo cliente, deberia ser "at" pero no lo podemos usar. Deberia guardar la data en un archivo temporal y luego reescribir el archivo original
+        archTemp = open("archivos\\ejercicioGuiaT\\listadoTemp.txt", "wt")
+        nombre = input("Ingrese el nombre del cliente: ")
+        telefono = input("Ingrese el telefono del cliente: ")
+        for line in arch.readlines():
+            archTemp.write(line)
+        arch.write(nombre + "," + telefono + "\n")
+        arch.close()
+        archTemp.close()
+    except FileNotFoundError:
+        print("El archivo no existe")
+    except:
+        print("Error desconocido")
+
+
+def eliminarTelefono():
+    try:
+        arch = open(path, "rt")
+        archTemp = open("archivos\\ejercicioGuiaT\\listadoTemp.txt", "wt")
+        nombre = input("Ingrese el nombre del cliente: ")
+        for line in arch.readlines():
+            if line.split(",")[0] != nombre:
+                archTemp.write(line)
+        arch.close()
+        archTemp.close()
+    except FileNotFoundError:
+        print("El archivo no existe")
+    except:
+        print("Error desconocido")
+def __main__():
+    print("Bienvenido al sistema de archivos")
+
+
+if __name__ == "__main__":
+    __main__()
--- archivos/ejercicioPrecios/index.py
+++ archivos/ejercicioPrecios/index.py
@@ -2,6 +2,7 @@ path = "archivos\\ejercicioPrecios\\"
 import random
 import string
 
+rubros = ["Perfumeria", "Limpieza", "Maquillaje"]
 def get_random_string(length):
     letters = string.ascii_lowercase #genera un caracter random de la a a la z
     result_str = ''.join(random.choice(letters) for i in range(length)) #genera un string de longitud length
@@ -12,8 +13,8 @@ def generaPrecios():
         arch = open(path + "precios.txt", "wt")
         for i in range(random.randint(10, 20)):
             new_str = get_random_string(8)
-            new_str2 = get_random_string(8)
-            arch.write(str(i) + ";" +  new_str  + ";" + new_str2 + ";" + str(i) + "\n")
+            robro = random.choice(rubros)
+            arch.write(str(i) + ";" +  new_str  + ";" + robro + ";" + str(i) + "\n")
         arch.close()
     except OSError:
         print("Error: FIle not found on directory")
@@ -37,10 +38,42 @@ def acumular20p():
     except:
         print("Error desconocido")
 
+def nuevosPrecios():
+    try:
+        arch = open(path + "precios.txt", "rt")
+        archP = open(path + "preciosP.txt", "wt")
+        archL = open(path + "preciosL.txt", "wt")
+        archM = open(path + "preciosM.txt", "wt")
+        maxPricex = ""
+        maxPrice = 0
+        for line in arch.readlines():
+            splitLine = line.split(";")
+            if splitLine[2] == "Perfumeria":
+                archP.write(splitLine[0] + ";" + splitLine[1] + ";" + str(float(splitLine[3]) * 1.1) + "\n")
+            elif splitLine[2] == "Limpieza":
+                archL.write(splitLine[0] + ";" + splitLine[1] + ";" + str(float(splitLine[3]) * 1.2) + "\n")
+            elif splitLine[2] == "Maquillaje":
+                archM.write(splitLine[0] + ";" + splitLine[1] + ";" + str(float(splitLine[3]) * 1.5) + "\n")
+                #find max price in Maquillaje
+                if float(splitLine[3]) * 1.5 > maxPrice:
+                    maxPrice = float(splitLine[3])  * 1.5
+                    maxPriceName = splitLine[1]
+        print("El producto mas caro de Maquillaje es: " + maxPriceName + " con un precio de: " + str(maxPrice))
+        arch.close()
+        archP.close()
+        archL.close()
+        archM.close()
+    except ValueError:
+        print("El numero ingresado no es valido")
+    except OSError:
+        print("Error: FIle not found on directory")
+    except:
+        print("Error desconocido")
 
 def __main__():
-    acumular20p()
     generaPrecios()
+    acumular20p()
+    nuevosPrecios()
 
 
 
