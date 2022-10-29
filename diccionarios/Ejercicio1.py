cfb394e8351c323adf1c6b91506a0fa6b7231b45
MatiasRetondo <matiasezeretondo@gmail.com>
2022-10-28 21:13:30 -0300

    fix

--- diccionarios/Ejercicio1.py
+++ diccionarios/Ejercicio1.py
@@ -1,11 +1,8 @@
 # realizar un programa para ingresar una frase y mostrar un listado ordenado alfabeticamente con las palabras que contiene, 
 # eliminando las repetidas y añadiendo la cantidad de veces que aparece cada una.
 
-def ingresoFrase():
+def ordenarPalabras():
     frase = input("Ingrese una frase: ")
-    return frase
-
-def ordenarPalabras(frase):
     palabras = frase.split(" ")
     palabras.sort()
     return palabras
@@ -18,12 +15,15 @@ def formateoParaDic(palabras):
         else:
             diccionario[palabra] = 1
     print("palabra  -> cantidad")
-    for palabra in diccionario:
-        print(palabra, "->", diccionario[palabra])
+    claves = list(diccionario)
+    claves.sort()
+    for clave in claves:
+        print(clave, "aparece :", diccionario[clave], "veces")
+    # for palabra in diccionario:
+    #     print(palabra, "->", diccionario[palabra])
 
 def __main__():
-    frase = ingresoFrase()
-    palabras = ordenarPalabras(frase)
+    palabras = ordenarPalabras()
     formateoParaDic(palabras)
 
 if __name__ == "__main__":  # al hacer esto, le decimos que se ejecute automaticamente la funcion main al principio de ejecucion
